aa8ce9e9e8a88e985bb131519e6ef70fdad96c0a
Adriano Goncalves <amg1976@gmail.com>
2015-12-18 23:16:18 +0000

    Carousel datasource

--- tvOS-prototype/MainCollectionDatasource.swift
+++ tvOS-prototype/MainCollectionDatasource.swift
@@ -8,27 +8,16 @@
 
 import UIKit
 
-extension MainViewController: iCarouselDataSource {
-
-   func numberOfItemsInCarousel(carousel: iCarousel) -> Int {
+extension MainViewController: UICollectionViewDataSource {
+   
+   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
       return 3
    }
    
-   func carousel(carousel: iCarousel, viewForItemAtIndex index: Int, reusingView view: UIView?) -> UIView {
-      let image = UIImage(named: "Baby \(index+1)")
-      let imageView = UIImageView(image: image)
-      let view = UIView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: image!.size))
-      view.addSubview(imageView)
-      return view
+   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+      let cell = collectionView.dequeueReusableCellWithReuseIdentifier(ImageCell.cellIdentifier, forIndexPath: indexPath) as! ImageCell
+      cell.imageView.image = UIImage(named: "Baby \(indexPath.item%3 + 1)")
+      return cell
    }
    
-}
-
-extension MainViewController: iCarouselDelegate {
-   func carousel(carousel: iCarousel, valueForOption option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
-      switch option {
-      case .Wrap: return 1.0
-      default: return value
-      }
-   }
 }
--- tvOS-prototype/TopCollectionDatasource.swift
+++ tvOS-prototype/TopCollectionDatasource.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-extension MainViewController: UICollectionViewDataSource {
+extension MainViewController: iCarouselDataSource {
    
-   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+   func numberOfItemsInCarousel(carousel: iCarousel) -> Int {
       return 3
    }
+   
+   func carousel(carousel: iCarousel, viewForItemAtIndex index: Int, reusingView view: UIView?) -> UIView {
+      let image = UIImage(named: "Baby \(index%3+1)")
+      let imageView = UIImageView(image: image)
+      let view = UIView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: image!.size))
+      view.addSubview(imageView)
+      return view
+   }
+   
+   func numberOfPlaceholdersInCarousel(carousel: iCarousel) -> Int {
+      return 4
+   }
+   
+   func carousel(carousel: iCarousel, placeholderViewAtIndex index: Int, reusingView view: UIView?) -> UIView {
+      let image = UIImage(named: "Baby 1")
+      let v = UIView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: image!.size))
+      v.backgroundColor = UIColor.lightGrayColor()
+      return v
+   }
+   
+}
 
-   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-      let cell = collectionView.dequeueReusableCellWithReuseIdentifier(ImageCell.cellIdentifier, forIndexPath: indexPath) as! ImageCell
-      cell.imageView.image = UIImage(named: "Baby \(indexPath.item+1)")
-      return cell
+extension MainViewController: iCarouselDelegate {
+   
+   func carousel(carousel: iCarousel, valueForOption option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+      switch option {
+      case .Wrap: return 1.0
+      default: return value
+      }
    }
 
 }
